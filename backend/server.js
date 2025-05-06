990691d8f50dc6dbc1bf8c3fd6c3c4e7cc03902c
Karan <karanmohan44@gmail.com>
2025-05-06 18:00:43 +0530

    fixes

--- backend/server.js
+++ backend/server.js
@@ -32,12 +32,12 @@ const allowedOrigins = [
   }))
 
   app.use(cookieParser())
-  app.use("/user", userRouter)
-  app.post("/clerk", express.raw({ type: "*/*" }), clerkwebhooks)
-
   app.use(express.json())
   app.use(express.urlencoded({extended: true}))
 
+
+  app.use("/user", userRouter)
+  app.post("/clerk", clerkwebhooks)
 
 app.listen(PORT, () => {
     console.log("server started successfully")
